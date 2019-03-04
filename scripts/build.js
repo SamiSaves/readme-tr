54ef5c17c346ad646b67b5338c890bd5701505a6
risenforces <risenforces@gmail.com>
2019-03-04 22:25:44 +0500

    fix encoding problem

--- scripts/build.js
+++ scripts/build.js
@@ -11,12 +11,12 @@ const md = new MarkdownIt({
   slugify: uslug
 });
 
-const mdContents = fs.readFileSync("source.md", "utf8");
+const mdContents = fs.readFileSync("source.md", "latin1");
 const mdHTML = md.render(mdContents);
 
-const template = fs.readFileSync("template.html", "utf8");
+const template = fs.readFileSync("template.html", "latin1");
 var finalHTML = template.replace(/{MD-RESULT}/g, mdHTML);
 
 fs.writeFileSync("index.html", finalHTML, { flag: "w" });
 
-console.log("Success!")
+console.log("Success!");
